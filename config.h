696530db10cdc5f36edc0bc089416fc994b8f5e1
Prasun Dutta <prasun@Milkyway-2.local>
2019-01-13 22:23:41 +0530

    Adding files .

--- config.h
+++ config.h
@@ -1,26 +1,26 @@
-/* include/learn.h.  Generated from learn.h.in by configure.  */
-/* include/learn.h.in.  Generated from configure.ac by autoheader.  */
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Name of package */
-#define PACKAGE "learn"
+#define PACKAGE "amhello"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "pdutta.phy@itbhu.ac.in"
+#define PACKAGE_BUGREPORT "bug-automake@gnu.org"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "learn"
+#define PACKAGE_NAME "amhello"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "learn 0.01"
+#define PACKAGE_STRING "amhello 1.0"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "learn"
+#define PACKAGE_TARNAME "amhello"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.01"
+#define PACKAGE_VERSION "1.0"
 
 /* Version number of package */
-#define VERSION "0.01"
+#define VERSION "1.0"
